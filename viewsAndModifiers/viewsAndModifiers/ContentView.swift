7de14dfcc26001ffb11902af8953e8ed8a9deaf0
fabricio.sffair <fabricio.sffair@resaas.com>
2020-07-01 11:16:05 -0700

    Started Project #3

---
+++ viewsAndModifiers/viewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  viewsAndModifiers
+//
+//  Created by Fabricio Ssfair on 7/1/20.
+//  Copyright © 2020 Fabricio Sperotto Sffair. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
