2c8df936b7a5257fd2f63bc3bfd7b07673babdf8
fabricio.sffair <fabricio.sffair@resaas.com>
2020-07-01 11:07:07 -0700

    [Guess the flag] Implemented Guess the flag game:
    * Clip Shapes, overlays and shadows
    * Gradients
    * VStack, HStack and ZStack
    * Alert

---
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  guessTheFlag
+//
+//  Created by Fabricio Ssfair on 6/30/20.
+//  Copyright © 2020 Fabricio Sperotto Sffair. All rights reserved.
+//
+
+import SwiftUI
+
+//#283048
+//
+//#859398
+
+
+struct ContentView: View {
+    
+    let darkBlue = Color(red: 40/255, green: 48/255, blue: 72/255)
+    let lightGrey = Color(red: 133/255, green: 147/255, blue: 152/255)
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingAlert = false
+    
+    @State private var score = 0
+    
+    @State private var scoreMessage = ""
+    @State private var scoreTitle = ""
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [darkBlue, lightGrey]), startPoint: .bottomTrailing, endPoint: .topLeading)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                Spacer()
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                Spacer()
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                        .clipShape(Capsule())
+                        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text("Score: \(score)")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .padding(.trailing)
+                }
+            }
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue")) {
+                    self.askQUestion()
+                })
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreMessage = "Nice going!"
+            score += 1
+        } else {
+            scoreTitle = "Wrong!"
+            scoreMessage = "That's the flag of \(countries[number])"
+            score = score == 0 ? score : score - 1
+        }
+        showingAlert = true
+    }
+    
+    func askQUestion() {
+        countries = countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
