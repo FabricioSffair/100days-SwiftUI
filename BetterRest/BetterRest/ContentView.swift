db2c182f3e85e5d0b62ac06a92db69c02350e899
Fabricio Sperotto Sffair <fabriciosffair@gmail.com>
2020-07-04 15:48:00 -0700

    BetterRest - Start project #4

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Fabricio Ssfair on 7/4/20.
+//  Copyright © 2020 Fabricio Sperotto Sffair. All rights reserved.
+//
+
+import SwiftUI
+import CoreML
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
