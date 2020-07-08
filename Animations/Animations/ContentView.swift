0d97d3d9abc016f9f6e7a964bf4fa6c4832137c3
Fabricio Sperotto Sffair <fabriciosffair@gmail.com>
2020-07-08 09:42:44 -0700

    Explicit animation with rotation3deffect

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
     
     var body: some View {
-        Text("")
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 10, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 2, y: 1, z: 1))
     }
 }
 
