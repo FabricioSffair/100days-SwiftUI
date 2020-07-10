9891ddb7720d53fe2327ee4936564159ce33cfa8
Fabricio Sperotto Sffair <fabricio.sffair@resaas.com>
2020-07-10 09:38:59 -0700

    Added animations to guesstheflag

--- guessTheFlag/guessTheFlag/ContentView.swift
+++ guessTheFlag/guessTheFlag/ContentView.swift
@@ -8,6 +8,42 @@
 
 import SwiftUI
 
+struct Opacity: ViewModifier {
+    let amount: Double
+    func body(content: Content) -> some View {
+        content.opacity(amount)
+    }
+}
+
+struct Rotate: ViewModifier {
+    let amount: Double
+    func body(content: Content) -> some View {
+        content
+            .rotation3DEffect(Angle(degrees: amount), axis: (x: 0, y: 1, z: 0))
+            .modifier(BorderColor(color: .green, width: 2))
+    }
+}
+
+struct BorderColor: ViewModifier {
+    let color: Color
+    let width: CGFloat
+    func body(content: Content) -> some View {
+        content.overlay(Capsule().stroke(color, lineWidth: width))
+    }
+}
+
+extension AnyTransition {
+    static var opacity25: AnyTransition {
+        .modifier(active: Opacity(amount: 1), identity: Opacity(amount: 0.25))
+    }
+    static var rotate360y: AnyTransition {
+        .modifier(active: Rotate(amount: 0), identity: Rotate(amount: 360))
+    }
+    static var greenBorder: AnyTransition {
+        .modifier(active: BorderColor(color: .black, width: 0), identity: BorderColor(color: .green, width: 2))
+    }
+}
+
 extension View {
     func capsuledWithBorder() -> some View {
         self.modifier(CapsuledBorder())
@@ -41,12 +77,14 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
-    @State private var showingAlert = false
-    
     @State private var score = 0
     
     @State private var scoreMessage = ""
     @State private var scoreTitle = ""
+    
+    @State private var highlightCorrect = false
+    @State private var highlightError = false
+    @State private var disableAction = false
     
     var body: some View {
         ZStack {
@@ -66,9 +104,24 @@ struct ContentView: View {
                 Spacer()
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        self.flagTapped(number)
+                        withAnimation {
+                            self.flagTapped(number)
+                        }
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            self.askQuestion()
+                        }
                     }) {
-                        FlagImage(imageName: self.countries[number])
+                        if self.highlightCorrect {
+                            FlagImage(imageName: self.countries[number])
+                                .transition(number == self.correctAnswer ? .rotate360y : .opacity25)
+                                .id(self.highlightCorrect)
+                        } else if self.highlightError {
+                            FlagImage(imageName: self.countries[number])
+                                .transition(number == self.correctAnswer ? .greenBorder : .opacity25)
+                                .id(self.highlightError)
+                        } else {
+                            FlagImage(imageName: self.countries[number])
+                        }
                     }
                 }
                 Spacer()
@@ -82,11 +135,6 @@ struct ContentView: View {
                 }
             }
         }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue")) {
-                    self.askQUestion()
-                })
-        }
     }
     
     func flagTapped(_ number: Int) {
@@ -94,15 +142,19 @@ struct ContentView: View {
             scoreTitle = "Correct"
             scoreMessage = "Nice going!"
             score += 1
+            self.highlightCorrect = true
         } else {
             scoreTitle = "Wrong!"
             scoreMessage = "That's the flag of \(countries[number])"
             score = score == 0 ? score : score - 1
+            self.highlightError = true
         }
-        showingAlert = true
+//        showingAlert = true
     }
     
-    func askQUestion() {
+    func askQuestion() {
+        self.highlightCorrect = false
+        self.highlightError = false
         countries = countries.shuffled()
         correctAnswer = Int.random(in: 0...2)
     }
