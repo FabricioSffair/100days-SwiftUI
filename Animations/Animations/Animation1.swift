8ab3367e1941c493198eeba46c79e684e5b7b1ce
Fabricio Sperotto Sffair <fabricio.sffair@resaas.com>
2020-07-10 08:58:23 -0700

    Animations
    * Created Custom transition
    * Explicit animations
    * Gestures
    * Animation orders

--- Animations/Animations/Animation1.swift
+++ Animations/Animations/Animation1.swift
@@ -8,6 +8,85 @@
 
 import SwiftUI
 
+//Transition
+struct Animation7: View {
+    
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    //PREVIEW DOES NOT WORK 100%, running on simulator works
+//                    .transition(.scale)
+            }
+        }
+    }
+}
+
+// Gesture
+struct Animation6: View {
+    
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 100))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged {self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+            }
+        )
+    }
+}
+
+// Gesture
+struct Animation5: View {
+    
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.spring()) {
+                            self.dragAmount = .zero
+                        }
+                }
+        )
+    }
+}
+
+// Animation order
 struct Animation4: View {
     
     @State private var enabled = false
@@ -26,6 +105,7 @@ struct Animation4: View {
     }
 }
 
+// Explicit animation
 struct Animation3: View {
     
     @State private var animationAmount = 0.0
@@ -44,6 +124,7 @@ struct Animation3: View {
     }
 }
 
+// Animation controlled
 struct Animation2: View {
     
     @State private var animationAmount: CGFloat = 1
@@ -66,6 +147,7 @@ struct Animation2: View {
     }
 }
 
+// Animation start
 struct Animation1: View {
     
     @State private var animationAmount: CGFloat = 1
--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,11 +8,45 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90,
+                                               anchor: .topLeading),
+                  identity: CornerRotateModifier(amount: 0,
+                                                 anchor: .leading))
+    }
+}
+
 struct ContentView: View {
     
+    @State private var isShowingRed = false
     
     var body: some View {
-        Text("Hello world")
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+                    //PREVIEW DOES NOT WORK 100%, running on simulator works
+//                    .transition(.scale)
+            }
+        }
     }
 }
 
